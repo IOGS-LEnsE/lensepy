55e26793262d7220d2a98fd44cf7a9864042343d
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-10-03 12:53:40 +0200

    Update CSS files - Version 0.2.1

--- src/lensepy/css.py
+++ src/lensepy/css.py
@@ -11,12 +11,15 @@ BLACK = '#FFFFFF'
 
 # Styles
 # ------
-styleH1 = f"font-size:20px; padding:7px; color:{BLUE_IOGS};font-weight: bold;"
-styleH2 = f"font-size:15px; padding:7px; color:{BLUE_IOGS}; font-weight: bold;"
-styleH3 = f"font-size:15px; padding:7px; color:{BLUE_IOGS};"
-styleCheckbox = f"font-size: 12px; padding: 7px; color: {BLUE_IOGS}; font-weight: normal;"
-no_style = f"background-color:{GRAY}; color:{BLACK}; font-size:15px;"
+styleH1 = f"font-size:18px; padding:3px; color:{BLUE_IOGS};font-weight: bold;"
+styleH2 = f"font-size:16px; padding:3px; color:{BLUE_IOGS}; font-weight: bold;"
+styleH3 = f"font-size:14px; padding:3px; color:{BLUE_IOGS};"
+styleCheckbox = f"font-size: 14px; padding: 3px; color: {BLUE_IOGS}; font-weight: normal;"
+no_style = f"background-color:{GRAY}; color:{BLACK}; font-size:14px;"
 
-disabled_button = f"background-color:{GRAY}; color:{BLACK}; font-size:15px; border-radius: 10px;"
-unactived_button = f"background-color:{BLUE_IOGS}; color:white; font-size:15px; font-weight:bold; border-radius: 10px;"
-actived_button = f"background-color:{ORANGE_IOGS}; color:white; font-size:15px; font-weight:bold; border-radius: 10px;"
+disabled_button = f"background-color:{GRAY}; color:{BLACK}; font-size:14px; border-radius: 10px;"
+unactived_button = f"background-color:{BLUE_IOGS}; color:white; font-size:14px; font-weight:bold; border-radius: 10px;"
+actived_button = f"background-color:{ORANGE_IOGS}; color:white; font-size:14px; font-weight:bold; border-radius: 10px;"
+
+BUTTON_HEIGHT = 35 #px
+OPTIONS_BUTTON_HEIGHT = 20 #pxn = f"background-color:{ORANGE_IOGS}; color:white; font-size:15px; font-weight:bold; border-radius: 10px;"
