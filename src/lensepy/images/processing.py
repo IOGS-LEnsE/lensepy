4b05cd7f16c5edaabf3e85e4424679466535c686
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-10-14 17:00:21 +0200

    Update to 0.2.5 / Adding image processing functions

--- src/lensepy/images/processing.py
+++ src/lensepy/images/processing.py
@@ -10,26 +10,21 @@ import cv2
 import numpy as np
 from PyQt6.QtGui import QImage
 
-def erode_image(array: np.ndarray, kernel: np.ndarray):
+def get_cross_kernel(size: int) -> np.ndarray:
     """
-    Return an eroded image.
-    :param array: Original image.
-    :param kernel: Kernel to use for erosion.
-    :return: Modified image.
+    Return a cross kernel.
+    :param size: Size of the kernel.
+    :return: Kernel.
     """
-    return cv2.erode(array, kernel, iterations=1)
+    return cv2.getStructuringElement(cv2.MORPH_CROSS, (size, size))
 
-# -*- coding: utf-8 -*-
-"""*procesing.py* file.
-
-*procesing* file contains image processes.
-
-.. moduleauthor:: Julien VILLEMEJANE <julien.villemejane@institutoptique.fr>
-"""
-import sys
-import cv2
-import numpy as np
-from PyQt6.QtGui import QImage
+def get_rect_kernel(size: int) -> np.ndarray:
+    """
+    Return a rectangular kernel.
+    :param size: Size of the kernel.
+    :return: Kernel.
+    """
+    return cv2.getStructuringElement(cv2.MORPH_RECT, (size, size))
 
 def erode_image(array: np.ndarray, kernel: np.ndarray):
     """
