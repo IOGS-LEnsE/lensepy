67e7f626958823c5e1e18c167207006b9fa01f3f
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-03-23 18:58:37 +0100

    Update Lensepy / Adding pyqt6 checkbox and progress bar

--- src/lensepy/__init__.py
+++ src/lensepy/__init__.py
@@ -81,3 +81,19 @@ def translate(key: str) -> str:
         return dictionary[key]
     else:
         return key
+
+
+def is_float(element: any) -> bool:
+    """
+    Return if any object is a float number.
+    :param element: Object to test.
+    :return: True if the object is a float number.
+    """
+    #If you expect None to be passed:
+    if element is None:
+        return False
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
--- src/lensepy/pyqt6/__init__.py
+++ src/lensepy/pyqt6/__init__.py
@@ -1,6 +1,8 @@
 __all__ = [
     "widget_slider",      # refers to the 'widget_slider.py' file
     "widget_histogram",   # refers to the 'widget_histogram.py' file
+    "widget_progress_bar",   # refers to the 'widget_progress_bar.py' file
+    "widget_checkbox",   # refers to the 'widget_checkbox.py' file
     "widget_image_display",     # refers to the 'widget_image_display.py' file
     "widget_image_histogram",     # refers to the 'widget_image_display.py' file
     "widget_combobox",
