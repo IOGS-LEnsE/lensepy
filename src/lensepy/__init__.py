c0732adafebca7bf5f15e28b5ecc1aa1148a791e
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-11-18 15:56:26 +0100

    Package 2.0.4

--- src/lensepy/__init__.py
+++ src/lensepy/__init__.py
@@ -6,7 +6,7 @@ __all__ = [
     'dictionary', # refers to the global variable dictionary
 ]
 
-version = '2.0.3'
+version = '2.0.4'
 print('SupOptique LEnsE Package (v.'+version+') / lensepy')
 import numpy as np
 import os
